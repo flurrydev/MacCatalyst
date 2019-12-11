8cf8261d069f8047af6a42775ee8733037874053
Hunter Hays <hhays@oath.com>
2019-12-11 11:30:01 -0800

    MacCatalyst bare bones sample

---
+++ catalinaTestApp/bridging-header.h
@@ -0,0 +1,13 @@
+//
+//  bridging-header.h
+//  catalinaTestApp
+//
+//  Created by Hunter Hays on 12/10/19.
+//  Copyright © 2019 Flurry. All rights reserved.
+//
+
+#ifndef bridging_header_h
+#define bridging_header_h
+#import "Flurry.h"
+
+#endif /* bridging_header_h */
